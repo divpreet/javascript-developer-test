c106096c5b5c90b15cb232383fe887cfd2933dda
Divpreet <divpreet.singh@objective.com>
2021-04-27 13:51:01 +1000

    Get arnie quote method implementation

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,30 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const getMessage = (body) => JSON.parse(body).message;
+const getSuccessQuote = (body) => {
+  return { 'Arnie Quote': getMessage(body)}
+}
+
+const getFailureQuote = (body) => {
+  return { 'FAILURE': getMessage(body)}
+}
+
+/**
+ *
+ * @param {string[]} urls The urls to be requested
+ * @returns {Promise} A promise which resolves to a results array.
+ */
+const getArnieQuotes = async (urls = []) => {
+  const promises = urls.map(async (url) => {
+    try {
+      const { status, body } = await httpGet(url);
+      return status === 200 ? getSuccessQuote(body) : getFailureQuote(body);
+    } catch (error) {
+      return getFailureQuote(error);
+    }
+  });
+
+  return Promise.all(promises);
 };
 
 module.exports = {
